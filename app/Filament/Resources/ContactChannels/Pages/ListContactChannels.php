ddac98e6f6189c5fd52e589d5110d0cb99457805
Scooller <scoollerx@hotmail.com>
2026-05-05 17:34:32 -0400

    Add CreateAction to ContactChannels list
    Expose a Create action in the ContactChannels list page header by importing Filament\Actions\CreateAction and returning it from getHeaderActions(). This enables the Create button on the ContactChannel resource list view.

--- app/Filament/Resources/ContactChannels/Pages/ListContactChannels.php
+++ app/Filament/Resources/ContactChannels/Pages/ListContactChannels.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\ContactChannels\Pages;
 
 use App\Filament\Resources\ContactChannels\ContactChannelResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListContactChannels extends ListRecords
 {
     protected static string $resource = ContactChannelResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
